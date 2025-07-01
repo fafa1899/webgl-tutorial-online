8e87efdb3ad9858ab8a10004475b570c7a89bf9d
fafa1899 <holybell44@126.com>
2025-07-01 19:50:39 +0800

    [A]配置HTTPS

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path"; // 导入 path 模块
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     server: {
       host: "0.0.0.0", // 绑定所有网络接口
       port: 3001, // 自定义开发服务器端口
       open: true, // 自动打开浏览器
-    //   https: {
-    //     key: resolve(__dirname, env.SSL_KEY), // 替换为你的密钥文件路径
-    //     cert: resolve(__dirname, env.SSL_CERT), // 替换为你的证书文件路径
-    //   },
+      https: {
+        key: resolve(__dirname, env.SSL_KEY), // 替换为你的密钥文件路径
+        cert: resolve(__dirname, env.SSL_CERT), // 替换为你的证书文件路径
+      },
     },
     build: {
       outDir: "./dist", // 构建输出目录

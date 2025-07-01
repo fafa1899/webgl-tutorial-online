6ea5d8143621953c2e892451e4225d0faba14e3a
fafa1899 <holybell44@126.com>
2025-07-01 16:26:17 +0800

    [A]提交案例文件
    Signed-off-by: fafa1899 <holybell44@126.com>

---
+++ public/case/common.js
@@ -0,0 +1,16 @@
+function resizeCanvasToDisplaySize(canvas) {
+  const dpr = window.devicePixelRatio || 1;
+  const width = Math.floor(canvas.clientWidth * dpr);
+  const height = Math.floor(canvas.clientHeight * dpr);
+
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+    return true;
+  }
+  return false;
+}
+
+
+
+

65910c972fbb87469246434130c2cfa762e81a77
fafa1899 <holybell44@126.com>
2025-07-01 19:02:04 +0800

    [A]增加配置

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path"; // 导入 path 模块
+
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      host: "0.0.0.0", // 绑定所有网络接口
+      port: 3001, // 自定义开发服务器端口
+      open: true, // 自动打开浏览器
+    //   https: {
+    //     key: resolve(__dirname, env.SSL_KEY), // 替换为你的密钥文件路径
+    //     cert: resolve(__dirname, env.SSL_CERT), // 替换为你的证书文件路径
+    //   },
+    },
+    build: {
+      outDir: "../dist", // 构建输出目录  
+    },
+    preview: {
+      host: "0.0.0.0", // 绑定所有网络接口
+      port: 3001, // 预览服务器端口
+      open: true, // 自动打开浏览器
+    },
+  };
+});

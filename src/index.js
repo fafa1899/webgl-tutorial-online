06ef45588c7a25ae13f34efe196c0e9fe5a9d0e3
fafa1899 <holybell44@126.com>
2025-07-11 18:02:55 +0800

    [A]设置默认案例

--- src/index.js
+++ src/index.js
@@ -21,13 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
   //
   const search = window.location.search; // 获取 ? 后面的部分（包括 ?）
   const params = new URLSearchParams(search);
-  const id = params.get("id"); // 获取单个参数
+  let id = params.get("id"); // 获取单个参数
 
-  if (id === null) {
-    return;
-  }
-  if (id >= caseData.length) {
-    return;
+  if (id === null || id >= caseData.length) {
+    id = 3;
   }
 
   const target = document.querySelector(`.toc-item[data-id="${id}"]`);
